2c919c44cf08727887e532edab4decef9d0a290c
WinnerPindi <Winner.Mazonzika.Pindi@uqtr.ca>
2024-01-29 23:39:22 -0500

    Shoe detail component

---
+++ src/components/ShoeDetail.jsx
@@ -0,0 +1,38 @@
+import nike1 from "../assets/n1-min.png";
+
+export function ShoeDetail() {
+  return (
+    <div className="flex flex-col lg:flex-row-reverse space-y-4">
+      {/* Shoe Image */}
+      <div className="flex-1 ">
+        <div className="bg-gradient-to-br from-[#F637CF] via-[#E3D876] to-[#4DD4C6]">
+          <img src={nike1} alt="" />
+        </div>
+      </div>
+
+      <div className="flex-1 space-y-6 ">
+        {/* Shoe text details */}
+        <div className="text-5xl font-black md:text-9xl">Nike Air max 270</div>
+        <div className="font-medium md:text-xl">
+          {
+            "The Nike Air Max 270 is a lifestyle shoe that's sure to turn with it's vibrant color gradient."
+          }
+        </div>
+        <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
+
+        {/* Shoe Buttons and links */}
+        <div className="space-x-10">
+          <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
+            Add to bag
+          </button>
+          <a
+            href="#"
+            className="text-lg font-bold underline underline-offset-4"
+          >
+            View details
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+// Supports weights 200-900
+import '@fontsource-variable/nunito-sans';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import  {App} from './App.jsx'
